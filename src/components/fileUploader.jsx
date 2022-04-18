4ebf86e8bf3ab760ec6c6252719315f121c63389
Nick <nickkossolapov@gmail.com>
2022-04-18 20:26:35 +0100

    Use Web Audio API to play audio through right channel only

--- src/components/fileUploader.jsx
+++ src/components/fileUploader.jsx
@@ -2,32 +2,37 @@ import React, { useState } from 'react';
 
 export function FileUploader() {
   const [selectedFile, setSelectedFile] = useState();
-  const [isSelected, setIsFileSelected] = useState(false);
 
   const changeHandler = (event) => {
     setSelectedFile(event.target.files[0]);
-    setIsFileSelected(true);
   };
 
   const handlePlay = () => {
-    console.log(selectedFile);
     const reader = new FileReader();
 
     reader.readAsArrayBuffer(selectedFile);
 
-    reader.onload = function() {
+    reader.onload = async function () {
+      const context = new AudioContext();
       const blob = new Blob([reader.result])
 
       const audioUrl = window.URL.createObjectURL(blob);
-      let aud = new Audio(audioUrl);
-      aud.play();
+      const audio = new Audio(audioUrl);
+      const track = context.createMediaElementSource(audio);
+
+      const pannerOptions = { pan: 1 };
+      const panner = new StereoPannerNode(context, pannerOptions);
+
+      track.connect(panner).connect(context.destination);
+
+      audio.play();
     }
   };
 
   return(
     <div>
-      <input type="file" name="file" onChange={changeHandler} />
-      {isSelected ? (
+      <input type="file" name="file" onChange={changeHandler}/>
+      {selectedFile ? (
         <div>
           <p>Filename: {selectedFile.name}</p>
           <p>Filetype: {selectedFile.type}</p>
