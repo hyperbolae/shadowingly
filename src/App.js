449587295c93a8ca68f98a8344d3f0e45e8e9670
Regan-Koopmans <regan.koopmans@teampicnic.com>
2022-04-24 14:01:07 +0200

    Initial work on begin and end times

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import logo from './logo.svg';
 import './App.css';
 import React from 'react';
 import { FileUploader } from './components/fileUploader';
+import { AudioRecorder } from "./component/audioRecorder/audioRecorder";
 
 function App() {
   return (
     <div className="App">
       <FileUploader/>
+        <AudioRecorder />
     </div>
   );
 }
