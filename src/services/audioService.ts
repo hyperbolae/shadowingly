7afef4b002eea98917e605317bdd9cc239664e18
Nick <nickkossolapov@gmail.com>
2022-08-27 12:38:35 +0100

    De-over-engineer audio service a bit

--- src/services/audioService.ts
+++ src/services/audioService.ts
@@ -1,47 +1,48 @@
-import {Channel} from "../domain/constants";
+import {Channel} from "../domain/Channel";
 import {ReadFile} from "../utils/fileReader";
-import {ISourceSetter} from "./audioServiceWriter";
+import {IAudioServiceSources} from "./audioServiceHooks";
 
 const disconnect = (source: AudioNode | undefined) => source && source.disconnect();
 
 const outputNumber = 0;
 
-export interface IAudioService {
-  play(): void;
 
-  stop(): void;
-
-  getPlaybackSource(): AudioSource;
-
-  getRecordedSource(): AudioSource;
-
-  getPlaybackChannel(): Channel;
-
-  setPlaybackChannel(channel: Channel): Channel;
-}
-
-export class AudioService implements IAudioService, ISourceSetter {
-  context: AudioContext;
-  merger: ChannelMergerNode;
-  playbackSource: AudioSource;
-  recordedSource: AudioSource;
-  playbackBuffer: Buffer;
-  recordedBuffer: Buffer;
+export class AudioService implements IAudioServiceSources {
   playbackChannel = Channel.Dual;
+
+  private startTime = 0;
+  private context: AudioContext;
+  private readonly merger: ChannelMergerNode;
+  private playbackSource: AudioSource;
+  private recordedSource: AudioSource;
+  private playbackBuffer: Buffer;
+  private recordedBuffer: Buffer;
+
+  get currentTime() {
+    return this.context.currentTime - this.startTime;
+  }
+
+  getPlaybackDuration = () => this.playbackBuffer?.duration;
+  getRecordingDuration = () => this.playbackBuffer?.duration;
 
   constructor(context: AudioContext = new AudioContext()) {
     this.context = context;
     this.merger = context.createChannelMerger(2);
   }
 
-  play = () => {
+  private start = () => {
     this.stop();
     this.playbackSource = this.createSource(this.playbackBuffer);
     this.recordedSource = this.createSource(this.recordedBuffer);
     this.connectChannels();
 
+    this.startTime = this.context.currentTime;
     if (this.playbackSource) this.playbackSource.start();
     if (this.recordedSource) this.recordedSource.start();
+  }
+
+  play = () => {
+    this.start();
   }
 
   stop = () => {
@@ -67,7 +68,7 @@ export class AudioService implements IAudioService, ISourceSetter {
   getRecordedSource = () => this.recordedSource;
   setRecordedSource = async (file: File) => {
     this.recordedBuffer = await this.createAudioBuffer(file);
-    this.recordedSource =  this.createSource(this.recordedBuffer);
+    this.recordedSource = this.createSource(this.recordedBuffer);
   }
   clearRecordedSource = () => {
     this.stop();
