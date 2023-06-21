0d70d1643dfd1ff0cf8d5df68e6a7ad89a45ca44
Nick Kossolapov <nickkossolapov@gmail.com>
2023-06-21 12:12:59 +0100

    Fix build warnings

--- src/components/progressBar/ProgressBarContainer.tsx
+++ src/components/progressBar/ProgressBarContainer.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useEffect, useRef, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import { useSelector } from 'react-redux'
 import { RootState } from '../../app/store'
 import { useAudioService } from '../../audioService/hooks'
