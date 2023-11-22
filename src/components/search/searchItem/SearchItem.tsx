27c5d47b676568f7235d083896ab1e6201b6ffaf
Nick Kossolapov <nickkossolapov@gmail.com>
2023-11-22 20:47:05 +0000

    Add simplified sentence type

--- src/components/search/searchItem/SearchItem.tsx
+++ src/components/search/searchItem/SearchItem.tsx
@@ -1,30 +1,20 @@
 import { useAppDispatch } from '../../../app/hooks'
 import { setPlaybackFile } from '../../../app/playbackFileSlice'
 import { baseAudioUrl } from '../../../constants/tatoeba'
-import { Sentence } from '../../../types/tatoeba'
-
-
-function getAudioUrl(sentence: Sentence) {
-  if (sentence.audios.length > 0 && sentence.audios[0].id) {
-    return baseAudioUrl + sentence.audios[0].id
-  } else {
-    return '' // todo handle this case
-  }
-}
+import { Sentence } from '../../../types/sentence'
 
 export function SearchItem({sentence}: { sentence: Sentence }) {
   const dispatch = useAppDispatch()
 
   async function handleAudioPlay(sentence: Sentence) {
-    const data = getAudioUrl(sentence)
+    const data = baseAudioUrl + sentence.audio.id
     const audio = new Audio(data)
     await audio.play()
   }
 
   async function handleAudioSelected(sentence: Sentence) {
-    const url = getAudioUrl(sentence)
+    const url = baseAudioUrl + sentence.audio.id
     const response = await (await fetch(url)).blob()
-
     const audioFile = new File([response], 'selected-audio.mp3', {type: 'audio/mpeg'})
 
     dispatch(setPlaybackFile(audioFile))
--- src/components/shadowing/progressBar/ProgressBar.tsx
+++ src/components/shadowing/progressBar/ProgressBar.tsx
@@ -2,17 +2,17 @@ import React from 'react'
 import './ProgressBar.css'
 
 interface ProgressBarProps {
-  endPercentage?: number;
-  currentPercentage: number;
+  endPercentage?: number
+  currentPercentage: number
 }
 
-export function ProgressBar({ endPercentage = 1, currentPercentage }: ProgressBarProps) {
+export function ProgressBar({endPercentage = 1, currentPercentage}: ProgressBarProps) {
   const width = (Math.min(currentPercentage, endPercentage) * 100) / endPercentage
 
   return (
     <div className="border">
-      <div className="line" style={{ width: +endPercentage * 100 + '%' }}>
-        <div className="highlight" style={{ width: +width + '%' }}/>
+      <div className="line" style={{width: +endPercentage * 100 + '%'}}>
+        <div className="highlight" style={{width: +width + '%'}}/>
       </div>
     </div>
   )
